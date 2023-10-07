6b64a6da1f249e84e487877e399fc7eccf0b780b
HyPride <106470800+hypride@users.noreply.github.com>
2023-10-07 22:37:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,35 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, request, send_file, Response, render_template
 from pytube import YouTube
+import io
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Replace with a secure secret key.
-
-# Define the path for saving downloaded videos
-DOWNLOADS_FOLDER = os.path.expanduser("~/Downloads")
-
-# Ensure the downloads folder exists
-if not os.path.exists(DOWNLOADS_FOLDER):
-    os.makedirs(DOWNLOADS_FOLDER)
-
-# Define available resolution options
-RESOLUTION_OPTIONS = [
-    ('2160p', '2160p'),
-    ('1440p', '1440p'),
-    ('1080p', '1080p'),
-    ('720p', '720p'),
-    ('480p', '480p'),
-    ('360p', '360p'),
-]
 
 @app.route('/')
 def index():
-    return render_template('index.html', resolution_options=RESOLUTION_OPTIONS)
+    return render_template('index.html')
 
 @app.route('/download', methods=['POST'])
 def download():
-    if request.method == 'POST':
-        url = request.form['url']
-        resolution = request.form['resolution']
+    url = request.form['url']
+    resolution = request.form['resolution']
 
-        try:
-            yt = YouTube(url)
-            stream = None
-            
-            if resolution == '2160p':
-                stream = yt.streams.filter(res="2160p").first()
-            elif resolution == '1440p':
-                stream = yt.streams.filter(res="1440p").first()
-            elif resolution == '1080p':
-                stream = yt.streams.filter(res="1080p").first()
-            elif resolution == '720p':
-                stream = yt.streams.filter(res="720p").first()
-            elif resolution == '480p':
-                stream = yt.streams.filter(res="480p").first()
-            elif resolution == '360p':
-                stream = yt.streams.filter(res="360p").first()
-            
-            if stream:
-                stream.download(output_path=DOWNLOADS_FOLDER)
-                flash(f"Video '{yt.title}' downloaded successfully to Downloads folder in {resolution} resolution.", 'success')
-            else:
-                flash(f"No video stream available in {resolution} resolution.", 'error')
-        except Exception as e:
-            flash(f"An error occurred: {str(e)}", 'error')
-    
-    return redirect(url_for('index'))
+    try:
+        yt = YouTube(url)
+        video_stream = yt.streams.get_by_resolution(resolution)
+        
+        if not video_stream:
+            return "Video not available in the selected resolution."
+        
+        video_bytes = io.BytesIO()
+        video_stream.stream_to_buffer(video_bytes)
+        video_bytes.seek(0)
+
+        response = Response(video_bytes, content_type="video/mp4")
+        response.headers["Content-Disposition"] = f"attachment; filename={yt.title}.mp4"
+
+        return response
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=True)
